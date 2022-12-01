9b39555f774f1e6ffdbaac4fa614ef606869e916
Choibalsan <choiboosmile@gmail.com>
2022-12-01 09:12:49 +0800

    CSS module bolgoh

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -1,4 +1,10 @@
 import css from './style.module.css';
 
-const Toolbar = () => <div className={css.Toolbar}>Toolbar</div>;
+const Toolbar = () => (
+  <header className={css.Toolbar}>
+    <div>...</div>
+    <div>Лого</div>
+    <nav>Цэс</nav>
+  </header>
+);
 export default Toolbar;
