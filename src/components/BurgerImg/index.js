fddeebca4ae6493a904d1f4357da91f74a1cc5aa
Choibalsan <choiboosmile@gmail.com>
2022-12-01 11:28:58 +0800

    BurgerImg and BurgerIngredient hiilee

--- src/components/BurgerImg/index.js
+++ src/components/BurgerImg/index.js
@@ -1,4 +1,14 @@
+import BurgerIngredient from '../BurgerIngredient';
 import css from './style.module.css';
 
-const BurgerImg = (props) => <div className={css.BurgerImg}>BurgerImg</div>;
+const BurgerImg = (props) => (
+  <div className={css.BurgerImg}>
+    <BurgerIngredient type='bread-top' />
+    <BurgerIngredient type='salad' />
+    <BurgerIngredient type='cheese' />
+    <BurgerIngredient type='bacon' />
+    <BurgerIngredient type='meat' />
+    <BurgerIngredient type='bread-bottom' />
+  </div>
+);
 export default BurgerImg;
